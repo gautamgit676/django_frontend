8f70bfcbf2863dc66e39a47b068bf509e9e6e36e
gautamgit676 <gautamgit244@gmail.com>
2026-01-03 14:16:16 +0530

    intigarte backends

--- app/views.py
+++ app/views.py
@@ -25,3 +25,19 @@ def home(request):
             "data": ""
         }
     return render(request, "home.html", {"data": data})
+
+
+# create user frontend api
+def create_user(request):
+    url = f"{BACKEND_API_BASE}userform/"
+    try:
+        r = requests.post(url, timeout=5)
+        r.raise_for_status()
+        data = r.json()
+    except requests.exceptions.RequestException as e:
+        logger.error("Backend API error: %s", e)
+        data = {
+            "message": "Service unavailable",
+            "data": ""
+        }
+    return render(request, "userform.html", {"data": data})
