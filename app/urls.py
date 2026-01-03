9c0ba18c9cce05280482573b3e9a3ab64d1035b8
gautamgit676 <gautamgit244@gmail.com>
2026-01-03 16:12:28 +0530

    intigarte userf

--- app/urls.py
+++ app/urls.py
@@ -2,5 +2,5 @@ from django.urls import path, include
 from app.views import home,create_user
 urlpatterns = [
     path('home/', home, name='home'),
-    path('userforms/', create_user, name='create_user'),
+    path('userf/', create_user, name='create_user'),
 ]
