a42b90f92dc3c86185187343a96104bfc61edb93
gautamgit676 <gautamgit244@gmail.com>
2026-01-03 15:26:23 +0530

    intigarte userform

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
-from app.views import create_user, home
+from app.views import home, create_user
 urlpatterns = [
     path('home', home, name='home'),
-    path('userforms/', create_user, name='create_user'),
+    # path('userforms/', create_user, name='create_user'),
 ]
