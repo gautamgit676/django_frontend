31e285bf8689f7e88bd5e3c0ed1817c5ff418cd7
gautamgit676 <gautamgit244@gmail.com>
2026-01-04 17:45:33 +0530

    add student form

--- app/urls.py
+++ app/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path, include
-from app.views import home,create_user, studentforms,userdatas,mainpage
+from app.views import home,create_user, create_student,userdatas,mainpage
 urlpatterns = [
     path('home/', home, name='home'),
     path('', mainpage, name='mainpage'),
     path('userf/', create_user, name='create_user'),
     path('userdetails/', userdatas, name='userdetails'),
-    path('studentform/', studentforms, name='student'),
+    path('studentform/', create_student, name='student'),
 ]
