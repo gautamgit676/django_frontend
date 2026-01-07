19b0011dafa6e4b1e04c058cead9a56226dcb505
gautamgit676 <gautamgit244@gmail.com>
2026-01-07 15:58:07 +0530

    add some data

--- app/urls.py
+++ app/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('home/', home, name='home'),
     path('', mainpage, name='mainpage'),
     path('userf/', UserCreate, name='userf'),
-    path("demoapi/", demoapi, name="demoapi"   ),
+    # path("demoapi/", demoapi, name="demoapi"   ),
     # path('userdetails/', userdatas, name='userdetails'),
     # path('student/', create_student, name='student'),
 ]
