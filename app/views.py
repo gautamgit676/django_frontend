dc1a88d7760b0970ba9b304040adefffcbdb729e
gautamgit676 <gautamgit244@gmail.com>
2026-01-20 15:46:34 +0530

    add some code

--- app/views.py
+++ app/views.py
@@ -98,8 +98,9 @@ def User_Profile(request, pk):
         token = request.session.get("access")
         if not token:
             return redirect("userlogin")
+        userurl =  f"{BACKEND_API_BASE}me/"
 
-        url = f"{BACKEND_API_BASE}userprofiles/me/"
+        url = f"{BACKEND_API_BASE}userprofiles/{userurl}/"
 
         headers = {
             "Authorization": f"Bearer {token}"
